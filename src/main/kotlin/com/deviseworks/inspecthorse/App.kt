375ae6c28c2d5ca3e5785de1a6d8216588108c71
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-04-29 14:53:20 +0900

    コマンドとイベントを登録

--- src/main/kotlin/com/deviseworks/inspecthorse/App.kt
+++ src/main/kotlin/com/deviseworks/inspecthorse/App.kt
@@ -1,9 +1,14 @@
 package com.deviseworks.inspecthorse
 
+import com.deviseworks.inspecthorse.commands.InspectCommand
+import com.deviseworks.inspecthorse.events.RightClickHorseEvent
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class App: JavaPlugin() {
     override fun onEnable() {
+        getCommand("inspect")?.setExecutor(InspectCommand(this))
+        Bukkit.getServer().pluginManager.registerEvents(RightClickHorseEvent(), this)
         logger.info("Inspect Horse is ready.")
     }
 }
